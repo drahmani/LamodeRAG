878fcea292bed5f9680a92ef0fc513b8dafffb18
Donya <donya.rh@gmail.com>
2025-09-08 12:53:28 +0100

    Remove test_RAG.py from repository

--- test_RAG.py
+++
@@ -1,6 +0,0 @@
-from src.rag_gpu import generate_answer
-
-def test_generate_answer_returns_string():
-    query = "I have blue eyes. What should I wear?"
-    answer = generate_answer(query)
-    assert isinstance(answer, str)
